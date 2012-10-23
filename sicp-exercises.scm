8d19193952674267c16d1ba5496ca8af975c4150
Dan Vingo <danvingo@gmail.com>
2012-10-23 09:10:22 -0400

    exercise 2.4 and 2.5

--- sicp-exercises.scm
+++ sicp-exercises.scm
@@ -1223,3 +1223,30 @@ guess: 4.5555465521473675
 (define (rect-area rec)
   (* (length (top-seg rec))
      (length (left-seg rec))))
+
+; exercise 2.4
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+; eg:
+(define x 1)
+(define y 2)
+(cdr (cons x y))
+;Value: 2
+
+; exercise 2.5
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car z)
+  (define (helper a num-divisions)
+    (cond ((not (= (remainder a 2) 0)) num-divisions)
+	  (else (helper (/ a 2) (+ 1 num-divisions)))))
+  (helper z 0))
+
+(define (cdr z)
+  (define (helper a num-divisions)
+    (cond ((not (= (remainder a 3) 0)) num-divisions)
+	  (else (helper (/ a 3) (+ 1 num-divisions)))))
+  (helper z 0))
