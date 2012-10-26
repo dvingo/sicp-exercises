9951964d0120871e6b1a7cc2002839671221f238
Dan Vingo <danvingo@gmail.com>
2012-10-26 09:10:37 -0400

    exercise 2.6

--- sicp-exercises.scm
+++ sicp-exercises.scm
@@ -1250,3 +1250,29 @@ guess: 4.5555465521473675
     (cond ((not (= (remainder a 3) 0)) num-divisions)
 	  (else (helper (/ a 3) (+ 1 num-divisions)))))
   (helper z 0))
+
+;exercise 2.6
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+; derive one:
+(add-1 zero)
+(lambda (f) (lambda (x) (f ((zero f) x))))
+(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+(lambda (f) (lambda (x) (f x)))
+
+; derive two:
+(add-1 one)
+(lambda (f) (lambda (x) (f ((one f) x))))
+(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+(lambda (f) (lambda (x) (f (f x))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+; a and b are function of one argument
+(define (church-add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+; apply f b times then apply that function a times.
