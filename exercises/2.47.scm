e9900132e2cb6fcd0bfe9479a5b042ea530abd20
Dan Vingo <danvingo@gmail.com>
2013-04-26 15:12:24 -0400

    Updated frame functions to match names used in the text

--- exercises/2.47.scm
+++ exercises/2.47.scm
@@ -1,41 +1,41 @@
 (define (make-frame origin edge1 edge2)
   (list origin edge1 edge2))
 
-(define (origin frame)
+(define (origin-frame frame)
   (car frame))
 
-(define (edge1 frame)
+(define (edge1-frame frame)
   (cadr frame))
 
-(define (edge2 frame)
+(define (edge2-frame frame)
   (caddr frame))
 
 (define orig (make-vect 0 0))
 (define e1 (make-vect 8 3))
 (define e2 (make-vect 10 6))
 (define f (make-frame orig e1 e2))
-(origin f)
-(edge1 f)
-(edge2 f)
+(origin-frame f)
+(edge1-frame f)
+(edge2-frame f)
 
 ; Second implementation
 
 (define (make-frame origin edge1 edge2)
   (cons origin (cons edge1 edge2)))
 
-(define (origin frame)
+(define (origin-frame frame)
   (car frame))
 
-(define (edge1 frame)
+(define (edge1-frame frame)
   (cadr frame))
 
-(define (edge2 frame)
+(define (edge2-frame frame)
   (cddr frame))
 
 (define orig (make-vect 0 1))
 (define e1 (make-vect 8 4))
 (define e2 (make-vect 13 6))
 (define f (make-frame orig e1 e2))
-(origin f)
-(edge1 f)
-(edge2 f)
+(origin-frame f)
+(edge1-frame f)
+(edge2-frame f)
