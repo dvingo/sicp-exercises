366f1d7525ce419f182ec254af34d4cd36480c27
Dan Vingo <danvingo@gmail.com>
2013-05-02 14:13:08 -0400

    Exercise 2.50

---
+++ exercises/2.50.scm
@@ -0,0 +1,36 @@
+; from the text:
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame graphic-dev)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+	(painter
+	 (make-frame new-origin
+		     (sub-vect (m corner1) new-origin)
+		     (sub-vect (m corner2) new-origin))
+	 graphic-dev)))))
+
+(define (flip-vert painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 0.0)))
+
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
+
+(define (rotccw-180 painter)
+    (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 0.0)))
+
+(define (rotccw-270 painter)
+    (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 0.0)))
+
+
